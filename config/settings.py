a82ea75019f240dad95b75a988304411d14509d2
Shukurillo <126893294+shukurillo2008@users.noreply.github.com>
2026-02-06 15:06:32 +0500

    ASD

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static_deployed'
 
 # Media files (User uploads)
 MEDIA_URL = 'media/'
